3ffeddfb2aae4d7cb7a564970c2b611d919ccb4b
Matheus Araujo <matheusaraujo1@pm.me>
2025-08-24 00:06:33 -0300

    Format frontend

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import markdown
 import uvicorn
 
+# REVIEW consider renaming to something not called main
 from main import RAGPipeline
 
-
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 rag = RAGPipeline()
 rag.build_knowledge_base()
@@ -21,9 +24,11 @@ def get_home(request: Request):
 
 @app.post("/ask", response_class=HTMLResponse)
 def post_ask(request: Request, query: str = Form(...)):
-    answer = rag.query(query)
+    raw_answer = rag.query(query)
+    html_answer = markdown.markdown(raw_answer, extensions=["fenced_code", "tables"])
+
     return templates.TemplateResponse(
-        "fragments/answer.html", {"request": request, "answer": answer}
+        "fragments/answer.html", {"request": request, "answer": html_answer}
     )
 
 
